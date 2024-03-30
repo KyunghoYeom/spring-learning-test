35cf1eba97621157b67a99e05e845dc1538644fd
KyunghoYeom <hoyeom1269@naver.com>
2024-03-30 15:45:26 +0900

    core1 world메서드 수정 TODO: /hello 요청 시 resources/templates/static.html 페이지가 응답할 수 있도록 설정하세요. 요구사항대로 일부 코드 수정

--- spring-mvc-1/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-1/initial/src/main/java/cholog/MemberController.java
@@ -13,9 +13,13 @@ public class MemberController {
     @GetMapping("/hello")
     public String world(@RequestParam(value = "name", required = false, defaultValue ="World"
     )String name, Model model) {
+        if(name.isEmpty()){
+            return "static";
+        }
         model.addAttribute("name", name);
         return "hello";
         // TODO: /hello 요청 시 resources/templates/static.html 페이지가 응답할 수 있도록 설정하세요.
+        // /hello만 실행시 static.html어떻게 응답 되는지?(index는 welcome page인데 static은 따로 설정이 안되어있어서)
         // TODO: 쿼리 파라미터로 name 요청이 들어왔을 때 해당 값을 hello.html에서 사용할 수 있도록 하세요.
 
     }
